13091d3ef6250f68d4a099d2ea7930ecc14d9a11
Brent Deacon <brent.deacon@ourvacationcentre.com>
2015-05-28 10:12:47 +1000

    Fix Arrays

--- vendor/laravel/framework/src/Illuminate/Support/helpers.php
+++ vendor/laravel/framework/src/Illuminate/Support/helpers.php
@@ -362,7 +362,7 @@ if ( ! function_exists('class_uses_recursive'))
 	 */
 	function class_uses_recursive($class)
 	{
-		$results = [];
+		$results = array();
 
 		foreach (array_merge([$class => $class], class_parents($class)) as $class)
 		{
